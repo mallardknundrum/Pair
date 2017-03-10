a1ed7f68fd3887bb99a7b9e9ed44898f28ae7b5c
Jeremiah Hawks <mallardknundrum@gmail.com>
2017-03-10 10:24:26 -0700

    Everything working. Persistence with CloudKit. Time spent on project: 1:25 this morning, 1 Hour previously coming up with algorithms to shuffle the array and to break it into an array of pairs. 1 hour previously working on the logic for the tableview data source functions and for the logic on the switch. Total time, about 3:25

--- Pair6WeekChallange/NameListTableViewController.swift
+++ Pair6WeekChallange/NameListTableViewController.swift
@@ -19,28 +19,81 @@ class NameListTableViewController: UITableViewController {
         }
     }
     
+    // MARK: - ShuffleSwitch
+    
+    @IBAction func shuffleSwitchToggled(_ sender: UISwitch) {
+        if sender.isOn == false {
+            NameController.shared.isRandom = false
+        } else {
+            NameController.shared.isRandom = true
+            NameController.shared.shuffleNamesAndBreakIntoPairs()
+        }
+        tableView.reloadData()
+    }
+    
+    
+    // MARK: - Add Name
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "Add a name", message: "Enter a name to add to the list", preferredStyle: .alert)
+        var nameTextField: UITextField!
+        alertController.addTextField { (textField) in
+            nameTextField = textField
+        }
+        let addAction = UIAlertAction(title: "Add a name", style: .default) { (_) in
+            guard let name = nameTextField.text, !name.isEmpty else { return }
+            NameController.shared.createName(withName: name)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     // MARK: - Table view data source
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return <#rowCount#>
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "<#reuseIdentifier#>", for: indexPath)
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            tableView.deleteRows(at: [indexPath], with: .fade)
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if NameController.shared.isRandom {
+            return NameController.shared.shuffledArrays.count
+        } else {
+            return 1
         }
     }
     
-    // MARK: - Navigation
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if NameController.shared.isRandom {
+            return "Pair \(section + 1)"
+        } else {
+            return "Name List"
+        }
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if NameController.shared.isRandom {
+            return NameController.shared.shuffledArrays[section].count
+        } else {
+            return NameController.shared.nameArray.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nameCell", for: indexPath)
+        if NameController.shared.isRandom {
+            let name = NameController.shared.shuffledArrays[indexPath.section][indexPath.row]
+            cell.textLabel?.text = name.name
+        } else {
+            let name = NameController.shared.nameArray[indexPath.row]
+            cell.textLabel?.text = name.name
+        }
+        return cell
+    }
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        }
+//    }
 }
